7e03a07a04be6a0e9c3b883da9064cb53d00a8b0
NguyenPhanHuy <hn790078@gmail.com>
2023-09-25 23:21:13 +0700

    Project Tuần 03

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
+<?php
+require_once 'vendor/autoload.php';
+use Src\Route;
+Route::route_site();
